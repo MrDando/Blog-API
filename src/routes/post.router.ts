7f755ab79e3afbadbbbef72ae9d0e7864660f1dc
Danijel Stanic <danijel.stanich@gmail.com>
2021-10-22 23:56:47 +0200

    Added types to request and response objects

--- src/routes/post.router.ts
+++ src/routes/post.router.ts
@@ -1,42 +1,42 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import * as postController from '../controllers/post.controller'
 import * as commentController from '../controllers/comment.controller'
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req: Request, res: Response) => {
     postController.getPosts(req, res)
 })
 
-router.post('/', (req, res) => {
+router.post('/', (req: Request, res: Response) => {
     postController.createPost(req, res)
 })
 
-router.get('/:postid', (req, res) => {
+router.get('/:postid', (req: Request, res: Response) => {
     postController.getSinglePost(req, res)
 })
 
-router.put('/:postid', (req, res) => {
+router.put('/:postid', (req: Request, res: Response) => {
     postController.updatePost(req, res)
 })
 
-router.delete('/:postid', (req, res) => {
+router.delete('/:postid', (req: Request, res: Response) => {
     postController.deletePost(req, res)
 })
 
-router.get('/:postid/comments', (req, res) => {
+router.get('/:postid/comments', (req: Request, res: Response) => {
     commentController.getComments(req, res)
 })
 
-router.post('/:postid/comments', (req, res) => {
+router.post('/:postid/comments', (req: Request, res: Response) => {
     commentController.createComment(req, res)
 })
 
-router.put('/:postid/comments/:commentid', (req, res) => {
+router.put('/:postid/comments/:commentid', (req: Request, res: Response) => {
     commentController.editComment(req, res)
 })
 
-router.delete('/:postid/comments/:commentid', (req, res) => {
+router.delete('/:postid/comments/:commentid', (req: Request, res: Response) => {
     commentController.deleteComment(req, res)
 })
 
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,17 +1,17 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import * as userController from '../controllers/user.controller'
 
 const router = express.Router();
 
-router.get('/signup', (req, res) => {
+router.get('/signup', (req: Request, res: Response) => {
     userController.signup(req, res)
 })
 
-router.get('/login', (req, res) => {
+router.get('/login', (req: Request, res: Response) => {
     userController.login(req, res)
 })
 
-router.get('/logout', (req, res) => {
+router.get('/logout', (req: Request, res: Response) => {
     userController.logout(req, res)
 })
 

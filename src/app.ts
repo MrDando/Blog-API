2ce0be431b3b56fddee88a73eb6ebedb6db40ca0
Danijel Stanic <danijel.stanich@gmail.com>
2021-10-29 12:58:04 +0200

    Added input validation for signup route

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,8 @@ const PORT = process.env.PORT
 
 const app = express()
 
+app.use(express.json())
+
 // -------------- DATABASE ----------------
 
 const dbURL = process.env.DB_STRING;
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,30 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
+import { body, validationResult } from 'express-validator'
 
-export function signup (req: Request, res: Response) {
-    res.send('signup test')
-}
+export const signup = [
+    body('username')
+    .exists({checkFalsy: true}).withMessage('Username is required')
+    .isAlphanumeric().withMessage('Username can only contain letters and numbers')
+    .isLength({ min: 3}).withMessage('Username must contain at least 3 characters')
+    .trim()
+    .escape(),
+    body('password')
+    .exists({checkFalsy: true}).withMessage('Password is required')
+    .isAlphanumeric().withMessage('Password can only contain letters and numbers')
+    .isLength({ min: 5}).withMessage('Password must contain at least 5 characters')
+    .trim()
+    .escape(),
+    (req: Request, res: Response, next: NextFunction) => {
+        const errors = validationResult(req)
+        console.log(errors.array())
+
+        if (!errors.isEmpty()) {
+            res.json(errors.array())
+        } else {
+            res.send('All ok')
+        }
+    }
+]
 
 export function login (req: Request, res: Response) {
     res.send('login test')

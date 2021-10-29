668b205075336024ff16e0d32ff6ec9baf7583fd
Danijel Stanic <danijel.stanich@gmail.com>
2021-10-29 16:33:21 +0200

    Updated signup validation with password confirmation check and username availability check

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction} from 'express'
-import { body, validationResult } from 'express-validator'
+import { body ,validationResult } from 'express-validator'
 
 import User from '../models/user.model'
 
@@ -8,12 +8,24 @@ export const signUpValidationSchema = [
         .exists({checkFalsy: true}).withMessage('Username is required').bail()
         .isAlphanumeric().withMessage('Username can only contain letters and numbers')
         .isLength({ min: 3}).withMessage('Username must contain at least 3 characters')
+        .custom((username) => {
+            return User.findOne({ username: username }).then((user) => {
+                if (user) {
+                    throw new Error('Username is not available');
+                }
+            })
+        }).withMessage('Username already exists')
         .trim()
         .escape(),
     body('password')
         .exists({checkFalsy: true}).withMessage('Password is required').bail()
         .isAlphanumeric().withMessage('Password can only contain letters and numbers')
         .isLength({ min: 5}).withMessage('Password must contain at least 5 characters')
+        .trim()
+        .escape(),
+    body('password-confirm')
+        .exists({checkFalsy: true}).withMessage('Password confirmations is required').bail()
+        .custom((value, { req }) => value === req.body.password).withMessage('Passwords do not match').bail()
         .trim()
         .escape(),
 ]
@@ -39,6 +51,4 @@ export const createUser = function (req: Request, res: Response, next: NextFunct
 
         res.status(201).send('User created successfully')
     })
-
-    console.log(user)
 }

dad046630340bc67678b3dd37ac5cbbaaea97347
Danijel Stanic <danijel.stanich@gmail.com>
2021-10-31 18:02:52 +0100

    Changed user routes to post method instead of get

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -3,11 +3,11 @@ import * as userController from '../controllers/user.controller'
 
 const router = express.Router();
 
-router.get('/signup', userController.signup)
+router.post('/signup', userController.signup)
 
-router.get('/login', userController.login)
+router.post('/login', userController.login)
 
-router.get('/logout', userController.logout)
+router.post('/logout', userController.logout)
 
 
 export default router

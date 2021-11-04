3a4661df67d18c3b1e47420d7de6453aa190bb8d
Danijel Stanic <danijel.stanich@gmail.com>
2021-11-05 00:02:33 +0100

    Removed unnecessary if statements

--- src/middleware/userMiddleware.ts
+++ src/middleware/userMiddleware.ts
@@ -21,48 +21,43 @@ export const handleUserCreate = function (req: Request, res: Response, next: Nex
 
 export async function handleUserLogin (req: Request, res: Response, next: NextFunction) {
     let { username, password } = req.body;
-    let opts = {
-        expiresIn: "7d"
-    }
-    const [ error, user, message ] = await authenticateUser(username, password)
-    if (error) {
-        next(error)
-    } else {
-        if (user) {
-            const userJWTData = {
-                sub: user.username,
-                admn: user.isAdmin,
-                crtr: user.isCreator
-            }
-            const secret = process.env.ACCESS_TOKEN_SECRET
-            if (typeof secret !== 'undefined') {
-                const token = jwt.sign( userJWTData, secret, opts);
-                return res.status(200).json({
-                    message: "Auth Passed",
-                    token
-                })
-            }
-        }
 
-        return res.status(401).json({ message })
+    const [ error, user, message ] = await authenticateUser(username, password)
+    if (error) { return next(error) } 
+    if (user) {
+        const userJWTData = {
+            sub: user.username,
+        }
+        const secret = process.env.ACCESS_TOKEN_SECRET
+        if (typeof secret !== 'undefined') {
+            const opts = {
+                expiresIn: "7d"
+            }
+            const token = jwt.sign( userJWTData, secret, opts);
+            return res.status(200).json({
+                message: "Auth Passed",
+                token
+            })
+        }
     }
+
+    return res.status(401).json({ message })
 }
 
 export function authorizeUser(req: Request, res: Response, next: NextFunction) {
     const token = extractJWT(req)
     if (!token) {
-        res.status(401).json({ message: 'Please login to access this page'})
-    } else {
-        const secret = process.env.ACCESS_TOKEN_SECRET
-        if (typeof secret !== 'undefined') {
-            jwt.verify(token, secret, (err, authData) => {
-                if(err) {
-                  res.status(403).json({ message: 'Token is invalid'});
-                } else {
-                    res.locals.user = authData
-                    next()
-                }
-            })
-        }
+       return res.status(401).json({ message: 'Please login to access this page'})
+    }
+
+    const secret = process.env.ACCESS_TOKEN_SECRET
+    if (typeof secret !== 'undefined') {
+        jwt.verify(token, secret, (err, authData) => {
+            if(err) {
+                return res.status(403).json({ message: 'Token is invalid'});
+            }
+            res.locals.user = authData
+            next()
+        })
     }
 }

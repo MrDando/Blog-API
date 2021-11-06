05de0f2cd44d333d76ef137084425ba54a758e32
Danijel Stanic <danijel.stanich@gmail.com>
2021-11-06 13:04:23 +0100

    Created a middleware function that checks if user is admin or Post/Comment author

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,7 +1,8 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, request, Request, Response } from "express";
 
 import { createPostValidationSchema } from '../schemas/post.schemas'
 import validateResults from '../middleware/validateResults'
+import { checkIfAuthorized } from '../middleware/userMiddleware'
 import { checkPostIdValidity, handleGetPosts, handleCreatePost, handleGetSinglePost } from '../middleware/postMiddleware'
 
 export const getPosts = [
@@ -17,9 +18,13 @@ export const createPost = [
     handleCreatePost
 ]
 
-export function updatePost(req: Request, res: Response) {
-    res.send('Update post not implemented')
-}
+export const updatePost = [
+    checkPostIdValidity,
+    checkIfAuthorized('Post'),
+    (req: Request, res: Response) => {
+        res.send('Auth ok!')
+    }
+]
 
 export function deletePost(req: Request, res: Response) {
     res.send('Delete post not implemented')
--- src/middleware/userMiddleware.ts
+++ src/middleware/userMiddleware.ts
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken'
 import ApiError from '../errors/APIError'
 
 import User from '../models/user.model'
+import Post from '../models/post.model'
 import authenticateUser from './utils/authenticateUser'
 import extractJWT from './utils/extractJWT'
 
@@ -60,5 +61,33 @@ export function authorizeUser(req: Request, res: Response, next: NextFunction) {
             res.locals.user = authData
             next()
         })
+    }
+}
+
+export function checkIfAuthorized (objectType: "Post" | "Comment") {
+    return async function (req: Request, res: Response, next: NextFunction) {
+        const JWTData = res.locals.user
+        try {
+            const user = await User.findOne({ username: JWTData.sub })
+            let postOrComment
+            if (objectType = "Post") {
+                postOrComment = await Post.findById(req.params.postid).populate('author', 'username')
+            }
+            if (objectType = "Comment") {
+
+                // Currently does the same thing as post. Needs to be updated when Comment model is created.
+                postOrComment = await Post.findById(req.params.postid).populate('author', 'username')
+            }
+            if (!user) { return next(ApiError.forbidden('Error validating token'))}
+            if (!postOrComment) { return next(ApiError.notFound('Post with that id does not exist'))}
+    
+            const authorId = postOrComment.author._id.toString()
+            const userId = user._id.toString()
+            if (!(user.isAdmin || userId === authorId)) { return next(ApiError.forbidden('You are not authorized to update this post'))}
+    
+            next()
+        } catch (err) {
+            return next(ApiError.internal('Internal server error'))
+        }
     }
 }

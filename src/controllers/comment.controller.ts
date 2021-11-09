34f1d00057585adc7875596b29bf7428eb60ef83
Danijel Stanic <danijel.stanich@gmail.com>
2021-11-09 13:48:50 +0100

    Added create comment feature

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,12 +1,40 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
+
+import ApiError from "../errors/APIError";
+import { authorizeUser } from '../middleware/userMiddleware'
+import Comment from '../models/comment.model'
+import Post from '../models/post.model'
+import User from '../models/user.model'
 
 export function getComments(req: Request, res: Response) {
     res.send('Get comments not implemented')
 }
 
-export function createComment(req: Request, res: Response) {
-    res.send('Create comment not implemented')
-}
+export const createComment = [
+    authorizeUser,
+    async function handleCreateComment(req: Request, res: Response, next: NextFunction) {
+        const JWTData = res.locals.JWT
+        const postId = req.params.postid
+
+        const user = await User.findOne({ username: JWTData.sub })
+        const post = await Post.findById(postId)
+
+        if (!user) { return next(ApiError.forbidden('Error validating token'))}
+        if (!post) { return next(ApiError.badRequest('Post with that ID does not exist'))}
+
+        const comment = new Comment({
+            text: req.body.text,
+            author: user.id,
+            post: post.id,
+        })
+
+        comment.save((err: any) => {
+            if (err) { return next (ApiError.internal('Internal server error')) }
+
+            res.status(201).send('Comment created successfully')
+        })
+    }
+]
 
 export function editComment(req: Request, res: Response) {
     res.send('Edit comment not implemented')
